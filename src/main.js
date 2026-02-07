2b44b5b90199c7398bbceddd4b01f9102a671f50
VIKRAM2605 <vikrams.al24@gmail.com>
2026-02-07 10:14:37 +0530

    added flappy and tappign motion

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { flappyBirdSpriteSheet, gameLoop } from "./character.js";
+
+export let gameRunning = true;
+
+// document.getElementById('game_container').addEventListener('click', () => {
+//     gameRunning = true
+
+//     if (gameRunning)
+//         gameLoop();
+// })
+
