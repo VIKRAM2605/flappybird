9eb003be25e439132f37bbf4a6bd289e39ed1e3e
VIKRAM2605 <vikrams.al24@gmail.com>
2026-02-12 11:18:51 +0530

    added correct width calculation for number placement for the best score and made stop animation before calling the same animation to prevent the game from bugging

--- src/main.js
+++ src/main.js
@@ -86,11 +86,13 @@ export function stopFirstTapAnimation() {
 export function toggleScene(game_running) {
     if (game_running && !firstTapped) {
         stopStartScreen();
+        stopFirstTapAnimation();
         firstTapAnimationId = requestAnimationFrame(startGameLoopWaitingForFirstTap);
     }
     else if (game_running) {
         stopStartScreen();
         stopFirstTapAnimation();
+        stopGameAnimation();
         requestAnimationFrame(gameLoop);
     } else {
         lastTime = performance.now();
@@ -206,6 +208,9 @@ export function isClickOnStartButton(mouseX, mouseY) {
         mouseY <= 150 + 14
     );
 };
+
+canvas.tabIndex = 0;
+canvas.focus();
 
 canvas.addEventListener('click', (e) => {
     const mousePos = getMouse(e);
